2dc199ab9ff8452b57d1bce4f6f115d063f6e5d6
pphila <efl2ice89@gmail.com>
2022-04-23 15:56:09 -0700

    it will pass fifth test where it will replace the number 3 with a phrase

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,14 @@
 function puppets(str) {
   let phraseArray = Array.from(str);
   for (let i = 0; i <= str; i++); {
-    if (str.toString().includes(1)) {
-      phraseArray.push("Beep!");
+    if (str.toString().includes(3)) {
+      phraseArray.push("Won't you be my neighbor?");
     } 
     else if (str.toString().includes(2)) {
       phraseArray.push("Boop!")
+    } 
+    else if (str.toString().includes(1)) {
+      phraseArray.push("Beep!")
     }
     else {
       phraseArray.push(str);
