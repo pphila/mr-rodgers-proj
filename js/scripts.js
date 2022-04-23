3e2bf57c3efdf3048b93703afd2d1afab88f1859
pphila <efl2ice89@gmail.com>
2022-04-23 16:55:19 -0700

    WIP for sixth test with function refactoring

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,20 @@
 //  Business Logic
 
-function puppets(str) {
-  let phraseArray = Array.from(str);
-  for (let i = 0; i <= str; i++); {
-    if (str.toString().includes(3)) {
+function puppets(input) {
+  let phraseArray = [];
+  //ReferenceError: i is not defined
+  for (let i = 0; i <= input.length; i++); {
+    if (i.toString().includes(3)) {
       phraseArray.push("Won't you be my neighbor?");
-    } 
-    else if (str.toString().includes(2)) {
-      phraseArray.push("Boop!")
-    } 
-    else if (str.toString().includes(1)) {
-      phraseArray.push("Beep!")
-    }
-    else {
-      phraseArray.push(str);
+    } else if (i.toString().includes(2)) {
+      phraseArray.push("Boop!");
+    } else if (i.toString().includes(1)) {
+      phraseArray.push("Beep!");
+    } else {
+      phraseArray.push(i);
     }
   }
   return phraseArray;
 }
 
 // UI Logic
-
