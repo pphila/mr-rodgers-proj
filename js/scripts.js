c773751786ff93cf0ee10db9bd3713ba1759595f
pphila <efl2ice89@gmail.com>
2022-04-23 15:28:18 -0700

    it will pass the fourth test replacing 2 with Boop!

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,9 @@ function puppets(str) {
   for (let i = 0; i <= str; i++); {
     if (str.toString().includes(1)) {
       phraseArray.push("Beep!");
-    } else if () {
-      
+    } 
+    else if (str.toString().includes(2)) {
+      phraseArray.push("Boop!")
     }
     else {
       phraseArray.push(str);
