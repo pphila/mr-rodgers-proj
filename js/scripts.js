454efe0261c8fcaf9bf883d5517255ff8ed6d197
pphila <efl2ice89@gmail.com>
2022-04-23 20:05:00 -0700

    add fix to typo in business logic

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,12 @@
 function puppets(number) {
   let returnArray = [];
   for (i = 0; i <= number; i++) {
-    if ([i].toString().includes(3)) {
-      returnArray.push("Won't you be my neighbor?");
+    if ([i].toString().includes(1)) {
+      returnArray.push("Beep!");
     } else if ([i].toString().includes(2)) {
       returnArray.push("Boop!");
-    } else if ([i].toString().includes(1)) {
-      returnArray.push("Beep!");
+    } else if ([i].toString().includes(3)) {
+      returnArray.push("Won't you be my neighbor?");
     } else {
       returnArray.push(i);
     }
